19e8254a84b3ae53cbba5bba3702a7addfea4c21
unknown <won03289@gmail.com>
2024-08-08 10:39:40 +0900

    24_08_08

---
+++ Algorithm_study/stack2/liveclass/backtracking.py
@@ -0,0 +1,66 @@
+def is_safe(maze, x, y, solution):
+    """
+    미로에서 (x, y) 위치가 안전한지 확인
+    """
+    n = len(maze)
+    return 0 <= x < n and 0 <= y < n and maze[x][y] == 0 and solution[x][y] == 0
+
+
+def solve_maze_util(maze, x, y, solution):
+    """
+    (x, y) 위치에서 미로를 해결하는 유틸리티 함수
+    """
+    n = len(maze)
+    # 목적지에 도달했는지 확인
+    if x == n - 1 and y == n - 1 and maze[x][y] == 0:
+        solution[x][y] = 1
+        return True
+
+    if is_safe(maze, x, y, solution):
+        # 현재 위치를 해결 경로에 포함
+        solution[x][y] = 1
+
+        # 아래로 이동
+        if solve_maze_util(maze, x + 1, y, solution):
+            return True
+
+        # 오른쪽으로 이동
+        if solve_maze_util(maze, x, y + 1, solution):
+            return True
+
+        # 만약 이동이 실패하면 해결 경로에서 제외 (백트래킹)
+        solution[x][y] = 0
+
+    return False
+
+
+def solve_maze(maze):
+    """
+    미로 문제를 해결하는 함수
+    """
+    n = len(maze)
+    solution = [[0 for _ in range(n)] for _ in range(n)]
+
+    if not solve_maze_util(maze, 0, 0, solution):
+        print("Solution doesn't exist")
+        return None
+
+    return solution
+
+
+# 예제 미로
+maze = [
+    [0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 0],
+    [0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0]
+]
+
+solution = solve_maze(maze)
+
+if solution:
+    for row in solution:
+        print(row)
+else:
+    print("No solution found")
---
+++ Algorithm_study/stack2/liveclass/calculator_1.py
@@ -0,0 +1,55 @@
+def infix_to_postfix(expression):
+    # 연산자의 우선순위를 정의
+    precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+    output = []  # 후위 표기법으로 변환된 수식을 저장할 리스트
+    operators = []  # 연산자를 저장할 스택
+
+    for char in expression:
+        if char.isnumeric():  # 피연산자인 경우
+            output.append(char)  # 출력 리스트에 추가
+        elif char in precedence:  # 연산자인 경우
+            while (operators and operators[-1] != '(' and
+                   precedence[operators[-1]] >= precedence[char]):
+                output.append(operators.pop())  # 스택에서 연산자를 꺼내 출력 리스트에 추가
+            operators.append(char)  # 현재 연산자를 스택에 추가
+        elif char == '(':  # 여는 괄호인 경우
+            operators.append(char)  # 스택에 추가
+        elif char == ')':  # 닫는 괄호인 경우
+            while operators and operators[-1] != '(':
+                output.append(operators.pop())  # 여는 괄호를 만날 때까지 스택에서 연산자를 꺼내 출력 리스트에 추가
+            operators.pop()  # 여는 괄호를 스택에서 제거
+
+    while operators:  # 스택에 남아있는 모든 연산자를 출력 리스트에 추가
+        output.append(operators.pop())
+
+    return ''.join(output)  # 리스트를 문자열로 변환하여 반환
+
+
+def evaluate_postfix(expression):
+    stack = []  # 피연산자를 저장할 스택
+
+    for char in expression:
+        if char.isnumeric():  # 피연산자인 경우
+            stack.append(int(char))  # 스택에 추가
+        else:  # 연산자인 경우
+            right = stack.pop()  # 오른쪽 피연산자를 스택에서 꺼냄
+            left = stack.pop()  # 왼쪽 피연산자를 스택에서 꺼냄
+            if char == '+':
+                stack.append(left + right)  # 더한 결과를 스택에 추가
+            elif char == '-':
+                stack.append(left - right)  # 뺀 결과를 스택에 추가
+            elif char == '*':
+                stack.append(left * right)  # 곱한 결과를 스택에 추가
+            elif char == '/':
+                stack.append(left / right)  # 나눈 결과를 스택에 추가
+            elif char == '^':
+                stack.append(left ** right)  # 제곱한 결과를 스택에 추가
+
+    return stack[0]  # 최종 결과를 반환
+
+# 예제 사용
+expression = "3+5*(2-8)"
+postfix_expression = infix_to_postfix(expression)
+print("후위 표기법:", postfix_expression)  # "3528-*+"
+result = evaluate_postfix(postfix_expression)
+print("계산 결과:", result)  # -25
