aa9e76184953bdf0418c9a1dd315e7fc58ae3876
Doh <won03289@gmail.com>
2024-08-26 16:25:39 +0900

    24_08_26

---
+++ Algorithm_study/DFS/dfs_questions.py
@@ -0,0 +1,44 @@
+# 음료수 얼려 먹기 문제
+
+# sol1)
+# DFS로 특정 노드를 방문하고 연결된 모든 노드들도 방문
+def dfs(x, y):
+    # 주어진 범위를 벗어나는 경우에는 즉시 종료
+    if x <= -1 or x >= N or y <= -1 or y >= M:
+        return False
+    # 현재 노드를 아직 방문하지 않았다면
+    if graph[x][y] == 0:
+        # 해당 노드 방문 처리
+        graph[x][y] = 1
+        # 상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        return True
+    return False
+
+
+N, M = map(int, input().split())  # N, M을 공백을 기준으로 구분하여 입력
+graph = [list(map(int, input())) for _ in range(N)]  # 2치원 리스트의 맵 정보 입력
+# 모든 노드(위치)에 대하여 음료수 채우기
+result = 0
+for i in range(N):
+    for j in range(M):
+        # 현재 위치에서 DFS 수행
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
+
+'''
+입력 예시
+4 5
+00110
+00011
+11111
+00000
+
+출력 예시
+3
+'''
