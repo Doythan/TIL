338f2ff9b7cef21bfb200c168732f2e707c08791
Doh <won03289@gmail.com>
2024-08-29 11:26:28 +0900

    24_08_29

--- Algorithm/Tree/1232_사칙연산/1232.py
+++ Algorithm/Tree/1232_사칙연산/1232.py
@@ -1,10 +1,66 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-for tc in range(1):
+
+def postorder(node_num):
+    # 현재 노드가 리프 노드(자식이 없는 경우)라면, 그 노드의 값을 반환
+    if not tree[node_num]:
+        return int(par_node[node_num])
+
+    # 왼쪽 자식과 오른쪽 자식을 후위 순회 방식으로 재귀적으로 방문
+    left = postorder(tree[node_num][0])
+    right = postorder(tree[node_num][1])
+
+    # 현재 노드의 연산자에 따라 왼쪽과 오른쪽 자식 노드의 값을 연산
+    if par_node[node_num] == '+':
+        return left + right
+    elif par_node[node_num] == '-':
+        return left - right
+    elif par_node[node_num] == '*':
+        return left * right
+    elif par_node[node_num] == '/':
+        return left / right
+
+
+# 총 10개의 테스트 케이스를 처리
+for tc in range(1, 11):
+    # 트리의 노드 수를 입력받음
     N = int(input())
+
+    # 각 노드의 연산자 또는 값을 저장하는 리스트 (1번 인덱스부터 사용)
+    par_node = [''] * (N + 1)
+
+    # 트리 구조를 저장하는 리스트, 각 노드는 두 자식을 가질 수 있음 (1번 인덱스부터 사용)
+
+    tree = [[] for _ in range(N + 1)]
+
+    # N개의 노드에 대한 정보를 입력받아 트리와 노드 정보 구성
     for _ in range(N):
         arr = list(input().split())
+        node_num = int(arr[0])  # 현재 노드 번호
+        par_node[node_num] = arr[1]  # 노드의 연산자 또는 값을 저장
+
+        # 자식 노드가 있는 경우 트리 구조에 자식 노드 정보 추가
+        if len(arr) == 4:
+            tree[node_num].append(int(arr[2]))  # 왼쪽 자식
+            tree[node_num].append(int(arr[3]))  # 오른쪽 자식
+
+    # 후위 순회를 통해 루트 노드(1번 노드)부터 계산을 시작하여 결과를 얻음
+    answer = int(postorder(1))
+
+    # 각 테스트 케이스의 결과를 출력
+    print(f'#{tc}', answer)
+
+'''
+5
+1 - 2 3
+2 - 4 5
+3 10
+4 88
+5 65
+'''
+
+
 
 
 
--- Algorithm/Tree/24_08_27_라이브(이진 트리 전위 순회)/tree_live.py
+++ Algorithm/Tree/24_08_27_라이브(이진 트리 전위 순회)/tree_live.py
@@ -1,4 +1,4 @@
-# 이진 트리 표현에 대하여 전위 순회하여 정점의 번호를 출력하기
+# 이진 트리 표현에 대하여 전위 순회, 중위 순회한 정점의 번호를 출력하는 코드
 
 def pre_order(T):
     if T:
@@ -7,31 +7,45 @@ def pre_order(T):
         pre_order(right[T])
 
 
-N = int(input())        # 1번부터 N번까지인 정점
-E = N-1
+def in_order(T):
+    if T:
+        in_order(left[T])
+        print(T, end=' ')
+        in_order(right[T])
+
+
+N = int(input())  # 1번부터 N번까지인 정점
+E = N - 1  # 간선의 수
 arr = list(map(int, input().split()))
-left = [0]*(N+1)        # 부모를 인덱스로 왼쪽자식번호 저장
-right = [0]*(N+1)       # 부모를 인덱스로 오른쪽자식번호 저장
-par = [0]*(N+1)         # 자식을 인덱스로 부모 저장
 
+left = [0] * (N + 1)  # 부모를 인덱스로 왼쪽 자식번호 저장(인덱스 0은 사용 x)
+right = [0] * (N + 1)  # 부모를 인덱스로 왼쪽 자식번호 저장(인덱스 0은 사용 x)
+parent = [0] * (N + 1)  # 자식을 인덱스로 부모 저장
+
+
+# 부모노드를 인덱스로 하여 왼쪽 자식은 left에 오른쪽 자식은 right에 넣음
 for i in range(E):
-    p, c = arr[i*2], arr[i*2+1]
-# for i in range(0,E*2, 2):
-#         p, c = arr[i], arr[i + 1]
-    if left[p] == 0:          # 왼쪽자식이 없으면
-        left[p] = c
+    par, chi = arr[i * 2], arr[i * 2 + 1]
+    if left[par] == 0:
+        left[par] = chi
     else:
-        right[p] = c
-    par[c] = p
+        right[par] = chi
+    parent[chi] = par  # 자식을 인덱스로 부모를 저장
 
-c = N
-while par[c] != 0:        # 부모가 있으면
-    c = par[c]          # 부모를 새로운 자식으로 두고
-root = c                # 더이상 부모가 없으면 root
-# print(root)
+# 루트(뿌리)를 찾기
+chi = N
+while parent[chi] != 0:
+    chi = parent[chi]
+root = chi
+
+print('전위 순회 :', end=' ')
 pre_order(root)
+
+print('\n중위 순회 :', end=' ')
+in_order(root)
 
 '''
 13
 1 2 1 3 2 4 3 5 3 6 4 7 5 8 5 9 6 10 6 11 7 12 11 13
 '''
+
