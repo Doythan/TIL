82a827fffa7b27228af95260e1c2f77e576dac4b
Gyoungwon <won03289@gmail.com>
2024-10-31 12:06:09 +0900

    update_1

--- django_study/django study8(update필요)/accounts/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-# 커스텀 User 모델로 대체하기 위한 class 작성
-class User(AbstractUser):
-    pass 
--- django_study/django3_4 study/().py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-article = Article()
-article
-article.title = 'first'
-article.content = 'django!'
-Article.objects.all()
